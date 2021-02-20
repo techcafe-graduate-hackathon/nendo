f57c83a9e6887f29d9a5e4864fafe7e0b419fa82
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-02-21 02:47:03 +0900

    add:origin

--- back/config/application.rb
+++ back/config/application.rb
@@ -35,6 +35,16 @@ module Myapp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+	# Permit cross origin
+config.middleware.insert_before 0, Rack::Cors do
+	allow do
+	  origins "*"
+	  resource "*",
+		headers: :any,
+		methods: [:get, :post, :options, :head]
+	end
+  end
+  config.api_only = true
+	
   end
 end
