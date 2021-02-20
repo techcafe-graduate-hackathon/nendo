f3a7177d209f28f6e7870da59b2db03f93fda4a8
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-02-21 03:12:26 +0900

    fix:CORSsetting

--- back/config/application.rb
+++ back/config/application.rb
@@ -35,16 +35,6 @@ module Myapp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-	# Permit cross origin
-config.middleware.insert_before 0, Rack::Cors do
-	allow do
-	  origins "'localhost:3000'"
-	  resource "*",
-		headers: :any,
-		methods: [:get, :post, :options, :head]
-	end
-  end
-  config.api_only = true
-	
+    config.api_only = true
   end
 end
