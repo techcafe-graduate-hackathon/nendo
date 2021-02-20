ff35efea4c46d5760c5d4b320224557a04072168
ShoyaSuzuki0523 <bellwood0523@gmail.com>
2021-02-21 01:38:20 +0900

    seedsの追加

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "test Tarou", email: "example@sample.com", password: "passward", password_confirmation: "password", is_developer: false)
