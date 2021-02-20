0626688dde3c951aef2cff0889c543aa3e689a7b
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-02-21 00:52:27 +0900

    add:schema

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_153201) do
+ActiveRecord::Schema.define(version: 2021_02_20_153656) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_02_20_153201) do
     t.string "function"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.bigint "idea_detail_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_ideas_on_category_id"
+    t.index ["idea_detail_id"], name: "index_ideas_on_idea_detail_id"
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -59,7 +73,9 @@ ActiveRecord::Schema.define(version: 2021_02_20_153201) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-
   end
 
+  add_foreign_key "ideas", "categories"
+  add_foreign_key "ideas", "idea_details"
+  add_foreign_key "ideas", "users"
 end
